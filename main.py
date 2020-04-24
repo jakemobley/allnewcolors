6863acf5e6ca0dae9ee27d9b21ff7abef9cf3c9d
Luco Aplepusci <jakemobley@gmail.com>
2020-04-23 19:18:42 -0500

    do I have to do this?

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.hmtl')
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
+
+if __name__ == '__main__':
+	app.run(debug=True)

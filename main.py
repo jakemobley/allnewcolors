bacbfc834513ca2e635450002d832137cf087843
Luco Aplepusci <jakemobley@gmail.com>
2020-04-23 20:20:31 -0500

    additions

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Jake Mobley',
+		'title'	: 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'April 22, 2020'
+	},
+	{
+		'author': 'Ariel Smith',
+		'title'	: 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'April 23, 2020'
+
+	}
+	]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
 	app.run(debug=True)

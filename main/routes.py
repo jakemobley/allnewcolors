f3c924707cfc64971e54e93933dce6ddf8cfcca9
Luco Aplepusci <jakemobley@gmail.com>
2020-04-27 14:45:00 -0500

    added new post creation pages and workflow

--- main/routes.py
+++ main/routes.py
@@ -3,30 +3,15 @@ import secrets
 from PIL import Image
 from flask import Flask, render_template, url_for, flash, redirect, request
 from main import app, db, bcrypt
-from main.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from main.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from main.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': 'Jake Mobley',
-        'title'	: 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 22, 2020'
-    },
-    {
-        'author': 'Ariel Smith',
-        'title'	: 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 23, 2020'
-
-    }
-]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -101,3 +86,15 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file = image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created.', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)

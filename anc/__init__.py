c2240ccfcbc0e3b2a6117ad01d99266e7e8c1624
Luco Aplepusci <jakemobley@gmail.com>
2020-04-30 21:57:57 -0500

    working custom error pages

--- anc/__init__.py
+++ anc/__init__.py
@@ -24,8 +24,11 @@ def create_app(config_class=Config):
     from anc.users.routes import users
     from anc.posts.routes import posts
     from anc.main.routes import main
+    from anc.errors.handlers import errors
+
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
-
+    app.register_blueprint(errors)
+    
     return app
